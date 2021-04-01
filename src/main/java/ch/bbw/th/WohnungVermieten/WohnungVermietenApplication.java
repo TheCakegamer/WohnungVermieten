f64cd845a7cd203b23964a4de25e0cee2622241f
Tim <40315936+TheCakegamer@users.noreply.github.com>
2021-04-01 09:46:33 +0200

    Initial commit

---
+++ src/main/java/ch/bbw/th/WohnungVermieten/WohnungVermietenApplication.java
@@ -0,0 +1,13 @@
+package ch.bbw.th.WohnungVermieten;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WohnungVermietenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WohnungVermietenApplication.class, args);
+	}
+
+}
